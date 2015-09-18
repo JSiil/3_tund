9c1797a832948dbd407c653e1467167d28721746
Jaagup Siil <jaagsii@greeny.cs.tlu.ee>
2015-09-18 12:51:32 +0300

    lehe parandus

---
+++ footer.php
@@ -0,0 +1,3 @@
+	<p><i>Lehe tegi Jaagup, 2015</i></p>
+</body>
+</html>
---
+++ page/home.php
@@ -0,0 +1,16 @@
+<?php
+
+	$page_title="Avaleht";
+	$file_name="home.php";
+?>
+<?php
+
+	//kopeerime header.php sisu
+	//../ - tähistavad, et fail asub ühes kaustas
+	require_once("../header.php");
+
+?>
+
+<h2>Avaleht</h2>
+
+<?php require_once("../footer.php"); ?>
